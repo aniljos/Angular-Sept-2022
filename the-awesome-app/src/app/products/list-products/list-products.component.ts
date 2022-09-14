5baec579193f3ba6a8c056d1cf90621960a845f3
aniljos <anil.jos@gmail.com>
2022-09-14 11:52:35 +0530

    Day 3

--- the-awesome-app/src/app/products/list-products/list-products.component.ts
+++ the-awesome-app/src/app/products/list-products/list-products.component.ts
@@ -14,12 +14,15 @@ export class ListProductsComponent implements OnInit {
 
   public products: Array<Product> = [];
   public searchKey: string = "";
+  public nProduct: Product = new Product(0, "", 0, "");
+  private url: string;
 
   constructor(private httpClient: HttpClient) { 
 
-      const url = "http://localhost:9000/products";
+      this.url = "https://aj-mock-rest-api.herokuapp.com/products"
+      //"http://localhost:9000/products";
       //async
-      const result = this.httpClient.get<Array<Product>>(url);
+      const result = this.httpClient.get<Array<Product>>(this.url);
      
       result.subscribe((data) =>{
         
@@ -36,6 +39,42 @@ export class ListProductsComponent implements OnInit {
   }
 
   ngOnInit(): void {
+  }
+
+  saveProduct(){
+
+        this.httpClient
+                .post(this.url, this.nProduct)
+                .subscribe(() => {
+
+                  this.products.push(this.nProduct);
+                  this.nProduct = new Product(0, "", 0, "");
+
+                  alert("Saved");
+                
+                }, () => {
+
+                  alert("Failed to save");
+                });
+
+  }
+
+  deleteProduct(product: Product){
+
+      this.httpClient
+          .delete(this.url + "/" + product.id)
+          .subscribe(() => {
+
+            const index = this.products.findIndex(item => item.id === product.id);
+            this.products.splice(index, 1);
+
+            alert("Deleted");
+
+          }, () => {
+
+            alert("Failed to delete")
+          })
+
   }
 
 }
